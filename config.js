8a8b722ce674d6a4618d31f490cfa80d6f5cde8f
You-sauce <yousauce274@gmail.com>
2026-02-16 21:00:28 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,32 @@
 module.exports = {
+    // ================= DATABASE =================
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://kaviduinduwara:kavidu2008@cluster0.bqmspdf.mongodb.net/soloBot?retryWrites=true&w=majority&appName=Cluster0",
+
+    // ================= BOT INFO =================
     BOT_NAME: "POP KID-MD",
     OWNER_NAME: "POP KID",
     OWNER_NUMBER: "254732297194",
     PREFIX: ".",
     MODE: "public",
-    PAIR_CODE: "POP-KID-1234", // <-- ton code pour connecter le bot
+    PAIR_CODE: "POP-KID-1234",
+
+    // ================= SYSTEM =================
     AUTO_REPLY: "true",
     AUTO_REACT: "true",
     AUTO_STICKER: "true",
     ALWAYS_ONLINE: "true",
+
+    // ================= SECURITY =================
     DELETE_LINKS: "true",
     ANTI_LINK: "true",
     ANTI_BAD: "true",
     ANTI_VV: "true",
-    ANTI_LINK: "true",
-    AUTO_REPLY: "true",
+
+    // ================= MEDIA =================
     MENU_IMAGE_URL: "https://files.catbox.moe/aapw1p.png",
     ALIVE_IMG: "https://files.catbox.moe/aapw1p.png",
+
+    // ================= STATUS =================
     LIVE_MSG: "> POP KID MD BOT ACTIVÉ ✅",
     DESCRIPTION: "*POP KID XTR BOT*",
     CUSTOM_REACT_EMOJIS: "💝,💖,💗,❤️",
