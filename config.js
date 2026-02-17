3144d9f98774bbc39e3e0c16bab5f0d808c4257e
You-sauce <yousauce274@gmail.com>
2026-02-16 20:19:23 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    BOT_NAME: "POP KID-MD",
+    OWNER_NAME: "POP KID",
+    OWNER_NUMBER: "254732297194",
+    PREFIX: ".",
+    MODE: "public",
+    PAIR_CODE: "POP-KID-1234", // <-- ton code pour connecter le bot
+    AUTO_REPLY: "true",
+    AUTO_REACT: "true",
+    AUTO_STICKER: "true",
+    ALWAYS_ONLINE: "true",
+    DELETE_LINKS: "true",
+    ANTI_LINK: "true",
+    ANTI_BAD: "true",
+    ANTI_VV: "true",
+    ANTI_LINK="true",
+    AUTO_REPLY="true",
+    MENU_IMAGE_URL: "https://files.catbox.moe/aapw1p.png",
+    ALIVE_IMG: "https://files.catbox.moe/aapw1p.png",
+    LIVE_MSG: "> POP KID MD BOT ACTIVÉ ✅",
+    DESCRIPTION: "*POP KID XTR BOT*",
+    CUSTOM_REACT_EMOJIS: "💝,💖,💗,❤️",
+    AUTO_STATUS_MSG: "*POP KID MD VIEWED*"
+};
