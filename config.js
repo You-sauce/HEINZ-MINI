456e1da7578058002b4fbac7f4a891feb4fad32c
You-sauce <yousauce274@gmail.com>
2026-02-16 21:15:44 -0600

    Update config.js

--- config.js
+++ config.js
@@ -15,14 +15,18 @@ module.exports = {
     AUTO_REACT: "true",
     AUTO_STICKER: "true",
     ALWAYS_ONLINE: "true",
+    READ_MESSAGE: "true", // Active le blue tick
 
     // ================= SECURITY =================
     DELETE_LINKS: "true",
     ANTI_LINK: "true",
     ANTI_BAD: "true",
     ANTI_VV: "true",
+    ANTI_CALL: "true", // Rejeter les appels
+    REJECT_MSG: "*📞 Call rejected automatically. No calls allowed.*",
 
     // ================= MEDIA =================
+    IMAGE_PATH: "https://files.catbox.moe/aapw1p.png", // Bienvenue / Alive image
     MENU_IMAGE_URL: "https://files.catbox.moe/aapw1p.png",
     ALIVE_IMG: "https://files.catbox.moe/aapw1p.png",
 
@@ -30,5 +34,9 @@ module.exports = {
     LIVE_MSG: "> POP KID MD BOT ACTIVÉ ✅",
     DESCRIPTION: "*POP KID XTR BOT*",
     CUSTOM_REACT_EMOJIS: "💝,💖,💗,❤️",
-    AUTO_STATUS_MSG: "*POP KID MD VIEWED*"
+    AUTO_STATUS_MSG: "*POP KID MD VIEWED*",
+    AUTO_VIEW_STATUS: "true", // Voir automatiquement les statuts
+    AUTO_LIKE_STATUS: "true", // Liker automatiquement
+    AUTO_STATUS_REPLY: "false", // Réponse automatique aux statuts
+    AUTO_LIKE_EMOJI: ["❤️","🌹","😇","💥","🔥","💫","💎","💙","🌝","💚"] // Liste des émojis pour liker
 };
